290406ccd4e4b92705c96f1edbdfd2871ad1255e
dantenvmt <thuannguyen.vm@gmail.com>
2022-01-12 22:04:08 -0600

    porn thing

---
+++ pornstar/Server/artifact/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+import utils
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+	return "hi"
+
+if __name__ ==  "__main__":
+	app.run(port= 5000)
